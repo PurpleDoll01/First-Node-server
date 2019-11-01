07599e1b187634e6c2dbdc7affcd1e02ef9ee7ce
Mariana Valencia <purple.mv@gmail.com>
2019-11-01 18:42:30 -0500

    Implementing Boom

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
 
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 //body parser
 app.use(express.json());
 
+//routes
 moviesApi(app);
+
+//catch 404
+app.use(notFoundHandler);
 
 //middlewares de manejo de errores
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen((config.port), function() {
